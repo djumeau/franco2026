0ad63647abdaede77278fe5a40b7b398fa226917
David Jumeau <djumeau@gmail.com>
2026-07-02 15:47:51 -0400

    HomeController... Corrected View return type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         return view('pages.index');
     }
