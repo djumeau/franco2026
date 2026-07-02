6eae47fe7e07ffab4e7ea5e93164803712803cc5
David Jumeau <djumeau@gmail.com>
2026-07-02 16:20:51 -0400

    Added layout component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Layout</title>
+</head>
+<body>
+    <h1>Content</h1>
 
+    {{ $slot }}
+    
+</body>
+</html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=], initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Conférence biblique francophone d'été 2026</title>
-</head>
-
-<body>
+<x-layout>
 
     <h1>Bienvenue à la conférence biblique francophone d'été 2026!</h1>
 
-</body>
-
-
-
-</html>
+</x-layout>
