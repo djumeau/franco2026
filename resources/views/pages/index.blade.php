586bd9f349a992e6c540e8cef411b556546d0df5
David Jumeau <djumeau@gmail.com>
2026-07-03 10:47:16 -0400

    Changes to index image properties.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,23 +8,28 @@
 
         <div class="p-4">
 
-            <h1 class="text-4xl md:text-5xl font-bold leading-10 md:leading-12 pb-4 md:pb-6">Bienvenue à la conférence biblique francophone d'été 2026!</h1>
+            <h1 class="text-4xl md:text-5xl font-bold leading-10 md:leading-14 pb-4 md:pb-6">Bienvenue à la conférence biblique francophone d'été 2026!</h1>
 
-            <div class="mx-auto px-6">
+            <div class="mx-4">
                 <p class="text-white text-base font-bold md:text-2xl italic">« Seigneur, à qui irions-nous ? Tu as les paroles de la vie éternelle. »</p>
-                <p class="text-white text-sm md:text-xl text-right md:pr-4">- Jean 6.68</p>
+                <p class="text-white text-sm md:text-xl text-right pr-4 md:pr-4">- Jean 6.68</p>
             </div>
 
             <!-- Dates and location -->
 
             <div class="mt-6 md:mt-8 text-lg md:text-xl">
-                <p class="mb-2">
-                    <span class="italic text-sm md:text-xl">Du 16 au 19 juillet 2026</span>
-                </p>
+
+                <div>
+                    <p class="pb-4">
+                        <span class="italic text-sm md:text-xl">Du 16 au 19 juillet 2026</span>
+                    </p>
+                </div>
+                
                 <div>
                     
-                   <p class="text-sm text-bold">CEGEP John Abbott</p>
-                   <p class="text-sm">Ste-Anne-de-Bellevue, Québec</p>
+                   <p class="text-sm md:text-2xl text-bold">CEGEP John Abbott</p>
+                   <p class="text-sm md:text-xl">Ste-Anne-de-Bellevue, Québec</p>
+
                 </div>
             </div>
 
@@ -34,9 +39,9 @@
 
             <div class="p-6 bg-slate-900/60 rounded-xl shadow-lg">
 
-                <img src="{{ asset('images/2022_cbu_conf_ete_groupe.jpg') }}" alt="Participants de la conférence biblique francophone d'été 2022" class="mx-auto rounded-xl w-9/10 pb-4">
+                <img src="{{ asset('images/2022_cbu_conf_ete_groupe.jpg') }}" alt="Participants de la conférence biblique francophone d'été 2022" class="mx-auto rounded-xl w-9/10">
 
-                <p>Ce thème a comme but de joindre des questions sur la vie souvent posées (parfois philosophiques)  autant par les non-croyants et les croyants, en fournissant des réponses bibliques qui pointent vers  Jésus. Les messages donneront des ou les pratiques sur comment vivre sa vie en Christ, ou  présenteront ce que ça veut dire être un chrétien (pour les nouveaux).</p>
+                <p class="pt-4">Ce thème a comme but de joindre des questions sur la vie souvent posées (parfois philosophiques) autant par les non-croyants et les croyants, en fournissant des réponses bibliques qui pointent vers  Jésus. Les messages donneront des ou les pratiques sur comment vivre sa vie en Christ, ou  présenteront ce que ça veut dire être un chrétien (pour les nouveaux).</p>
 
             </div>
 
