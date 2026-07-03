c0114bc85d3d9efb804e4ca54e26620432b528b7
David Jumeau <djumeau@gmail.com>
2026-07-02 20:09:36 -0400

    Changes to layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<html lang="fr">
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Layout</title>
-</head>
-<body>
-    <h1>Content</h1>
 
-    {{ $slot }}
+    <link rel="icon" type="image/svg+xml" href="{{ asset('logo_ubf_favicon.svg') }}">
+
+    <title>{{ $title ?? 'Conférence Biblique Francophone 2026' }}</title>
+</head>
+
+<body style="background-color: value(--color-background);">
+
+    <main>
+        {{ $slot }}
+    </main>
     
 </body>
+
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
 
-    <h1>Bienvenue à la conférence biblique francophone d'été 2026!</h1>
+    <x-slot name="title">
+        Conférence biblique francophone d'été 2026 | Acceuil
+    </x-slot>
+
+    <h1 style='color: var(--color-purpleui-500)'>Bienvenue à la conférence biblique francophone d'été 2026! 2</h1>
 
 </x-layout>
