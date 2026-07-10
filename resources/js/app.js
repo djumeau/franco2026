ee130034a3cd4d7519b08942f7ff8093eb9a92ae
David Jumeau <djumeau@gmail.com>
2026-07-10 10:15:58 -0400

    Added hamburger menu.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import "@fortawesome/fontawesome-free/js/all.js";
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector("#hamburger");
+    const menu = document.querySelector("#mobile-menu");
+
+    // Defend against errors if the elements don't exist on the current page
+    if (hamburger && menu) {
+        hamburger.addEventListener("click", function () {
+            console.log("show menu");
+            menu.classList.toggle("hidden");
+        });
+    }
+});
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,37 +1,80 @@
 @php
-    $currentLocale = app()->getLocale();
-    // Example languages: 'en' and 'fr'
-    $oppositeLang = $currentLocale === 'en' ? 'fr' : 'en';
+    $oppositeLang = app()->isLocale('en') === 'en' ? 'fr' : 'en';
+
 @endphp
 
-<nav
-    class="flex items-center border-b px-4 bg-black/50 justify-between py-4 backdrop-blur-md sticky top-0 left-0 z-50">
+<nav class="border-b px-4 bg-black/50 justify-between backdrop-blur-md sticky top-0 left-0 z-50">
 
-    <!-- Logo -->
-    <div class="flex items-center">
+    <div class="flex justify-between mx-auto px-4 py-4 md:px-6 h-full">
 
-        @if (request()->routeIs('index'))
-            <div>
-                <img src="{{ asset('icons/logo_ubf_white.svg') }}" alt="Logo" class="h-12" />
-            </div>
-            <span class="text-white font-bold text-xl md:text-2xl p-0 ml-1">
-                CBFE 2026
-            </span>
-        @else
-            <div>
-                <a href="{{ route('index') }}">
+        <!-- Logo -->
+        <div class="flex shrink-0 items-center">
+
+            @if (request()->routeIs('index'))
+                <div>
                     <img src="{{ asset('icons/logo_ubf_white.svg') }}" alt="Logo" class="h-12" />
-                </a>
-            </div>
-            <span class="text-white font-bold text-xl md:text-2xl hover:underline underline-offset-4 p-0 ml-1">
-                <a href="{{ route('index') }}">CBFE 2026</a>
-            </span>
-        @endif
+                </div>
 
+                <span class="text-white font-bold text-xl md:text-2xl p-0 ml-1">
+                    CBFE 2026
+                </span>
+            @else
+                <div>
+                    <a href="{{ route('index') }}">
+                        <img src="{{ asset('icons/logo_ubf_white.svg') }}" alt="Logo" class="h-12" />
+                    </a>
+                </div>
+
+                <span class="text-white font-bold text-xl md:text-2xl hover:underline underline-offset-4 p-0 ml-1">
+                    <a href="{{ route('index') }}">CBFE 2026</a>
+                </span>
+            @endif
+
+        </div>
+
+        <!-- Navigation Links - Desktop -->
+        <div id='desktop-menu' class="hidden md:flex items-center space-x-8 text-sm md:text-base text-white">
+
+            @if(request()->routeIs('program'))
+                <span class="font-bold">{{__('program.title')}}</span>
+            @else
+                <a href="{{route('program')}}" class="hover:text-white hover:underline underline-offset-2 transition-colors">{{__('program.title')}}</a>
+            @endif
+
+            @if(request()->routeIs('documents'))
+                <span class="font-bold">{{__('documents.title')}}</span>
+            @else
+                <a href="{{route('documents')}}" class="hover:text-white hover:underline underline-offset-2 transition-colors">{{__('documents.title')}}</a>
+            @endif
+
+            @if(request()->routeIs('prayertopics'))
+                <span class="text-base font-bold text-white">{{__('prayertopics.title')}}</span>
+            @else
+                <a href="{{route('prayertopics')}}" class="hover:text-white hover:underline underline-offset-2 transition-colors">{{__('prayertopics.title')}}</a>
+            @endif
+            
+            <a href="{{ route('lang.switch', ['locale' => $oppositeLang]) }}">
+                <i class="fa-solid fa-globe"></i> 
+                <span class="hover:underline underline-offset-4">{{ strtoupper($oppositeLang) }}</span>
+            </a>
+        
+        </div>
+
+        <div class="flex items-center md:hidden">
+            <button id='hamburger' class='text-white' type='button'>
+                <i class="fa fa-bars text-2xl"></i>
+            </button>
+        </div>
+    
     </div>
 
-    <!-- Navigation Links -->
-    <div class="flex items-center space-x-4 text-sm md:text-base text-white">
+</nav>
+
+<!-- Mobile Menu -->
+<div id='mobile-menu' class='sticky hidden flex-col top-20 left-0 right-0 md:hidden bg-slate-900 text-white/90 py-4 space-y-2 z-50'>
+    
+    <!-- Navigation Links - Mobile -->
+    <div class='flex flex-col pl-4'>
 
         @if(request()->routeIs('program'))
             <span class="font-bold">{{__('program.title')}}</span>
@@ -55,7 +98,7 @@
             <i class="fa-solid fa-globe"></i> 
             <span class="hover:underline underline-offset-4">{{ strtoupper($oppositeLang) }}</span>
         </a>
-        
+    
     </div>
 
-</nav>
+</div>
