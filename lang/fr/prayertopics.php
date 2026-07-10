68e70b131e6d0e3fedfc198473601e1324cf81c3
David Jumeau <djumeau@gmail.com>
2026-07-10 13:07:30 -0400

    Updated Prayer topics file.

--- lang/fr/prayertopics.php
+++ lang/fr/prayertopics.php
@@ -4,8 +4,8 @@ return [
     'title' => 'Sujets de prières',
 
     'download' => [
-            'title' => "Télécharger les sujets de prières",
-            'filepath' => "documents/2026_cbfe_priere.pdf",
+        'title' => "Télécharger les sujets de prières",
+        'filepath' => "documents/2026_cbfe_priere.pdf",
     ],
 
     'categories' => [
