f83fb77e5a5f08960a8107ca1c8685822a3282a9
David Jumeau <djumeau@gmail.com>
2026-07-10 13:05:14 -0400

    Updated Prayer Topics Language File.

--- lang/en/prayertopics.php
+++ lang/en/prayertopics.php
@@ -8,4 +8,20 @@ return [
         'filepath' => "documents/2026_cbfe_priere.pdf",
     ],
 
+    'categories' => [
+
+        'cat-1' => [
+            'name' => 'Conference Attendees',
+            'icon' => 'icons/logo_ubf_blue.svg',
+            'prayer-topics' => [
+                'items' => [
+                    'name' => 'Conference Program',
+                    'type' => 'pdf',
+                    'filename' => '2026_cbfe_programme.pdf',
+                ],
+            ],
+        ],
+    
+    ],
+
 ];
