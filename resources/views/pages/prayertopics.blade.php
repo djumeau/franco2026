d78d90575cde667c9c9c8b850e0612939bea309f
David Jumeau <djumeau@gmail.com>
2026-07-11 18:04:25 -0400

    prayertopics.blade.php corrected key verse link

--- resources/views/pages/prayertopics.blade.php
+++ resources/views/pages/prayertopics.blade.php
@@ -100,7 +100,7 @@
                             <div class='w-fit bg-slate-800 text-white shrink rounded-xl mt-2 p-3 px-3'>
                                 
                                 @if(!empty($card['key-verse-link']))
-                                    <a href="{{$card['key-verse-link']}}" class="hover:underline" _target="blank">{{$card['key-verse']}}</a>
+                                    <a href="{{$card['key-verse-link']}}" class="hover:underline" target="_blank">{{$card['key-verse']}}</a>
                                 @else
                                     {{$card['key-verse']}}
                                 @endif
