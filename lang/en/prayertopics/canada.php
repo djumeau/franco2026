79fc8d9431bf79345cfbb18fca6eaad783506993
David Jumeau <djumeau@gmail.com>
2026-07-12 10:02:38 -0400

    canada prayer topics updated - Sherbrooke ministry

--- lang/en/prayertopics/canada.php
+++ lang/en/prayertopics/canada.php
@@ -34,6 +34,37 @@ return [
                 ],
             ],
 
+            "card-2" => [
+                "title" => "Sherbrooke",
+                "icon" => "fa-solid fa-church",
+                "key-verse" => "John 15:5",
+                "key-verse-link" => "https://www.biblegateway.com/passage/?search=John%2015%3A5&version=NIV",
+                "prayer-topics" => [
+
+                    "prayer-1" => "That we may be rooted in Jesus according to John 15:5 : 'I am the vine; you are the branches. If you remain in me and I in you, you will bear much fruit; apart from me you can do nothing.'",
+                    
+                    "prayer-2" => "35 students from University of Sherbrooke (French) and Bishop's (English) to attend Sunday worship, eager to grow as disciples of Jesus. We give thanks to the Lord for providing us with a café in town (“Caffecino”) as our meeting place.",
+
+                    "prayer-3" => "A dedicated student committed to growing as a disciple of Jesus who has the ability to strengthen the ministry serving the French-speaking community.",
+
+                    "prayer-4" => "Two faithful students join Joshua and Céline every week for Bible study and to grow in their faith. Please pray for José and for God’s work in his life.",
+
+                    "prayer-5" => "To grow in a healthy and sustainable way, without burnout, unhealthy pressure, or dependence on a small number of people. Pray for wise structures, shared responsibilities, healthy families, strong relationships, and long-term spiritual fruit. Above all, pray for Joshua and Céline’s family and for all those who serve with perseverance, patience, and faith.",
+
+                    "prayer-6" => "Music Ministry - Due Dieu is calling on volunteer musicians, singers, and worship leaders to help establish a faithful, Christ-centered music and worship program.",
+
+                    "prayer-7" => "30 participants in the running event each week. The goal is to achieve good health, fellowship, physical discipline, and spiritual endurance.",
+
+                    "prayer-8" => "Pray for David, Clarise, Lida, Jordan, Nyla, Witness, Jovanna, Francis, Jaleh, Tahlia and all those who are connected to our ministry. We pray that each may grow personally in Christ and respond to God's call.",
+
+                    "prayer-9" => "We pray for the growth of our community to welcome everyone with hospitality for God's glory.",
+
+                    "prayer-10" => "We pray for wisdom and God's provision.",
+                    
+                ],
+                
+            ],
+
         ],
         
     ],
--- lang/fr/prayertopics/canada.php
+++ lang/fr/prayertopics/canada.php
@@ -34,30 +34,35 @@ return [
                 ],
             ],
 
-             "card-2" => [
+            "card-2" => [
                 "title" => "Sherbrooke",
                 "icon" => "fa-solid fa-church",
-                "key-verse" => "Éphésiens 4.15-16",
-                "key-verse-link" => "https://www.biblegateway.com/passage/?search=%C3%89ph%C3%A9siens%204%3A15-16&version=SG21",
+                "key-verse" => "Jean 15.5",
+                "key-verse-link" => "https://www.biblegateway.com/passage/?search=Jean%2015%3A5&version=SG21",
                 "prayer-topics" => [
+
+                    "prayer-1" => "Prions pour que l’UBF de Sherbrooke reste enracinée en Jésus et vive selon Jean 15.5 : « Je suis la vigne, vous êtes les sarments. Celui qui demeure en moi et en qui je demeure porte beaucoup de fruit, car sans moi, vous ne pouvez rien faire. »",
                     
-                    "prayer-1" => "Grandir comme une communauté remplie d'amour.",
+                    "prayer-2" => "35 étudiants parmi les universités de Sherbrooke (Francophone) et Bishop's (Anglais) à participer à l'adoration du dimanche et en ayant le désir de grandir comme disciples de Jésus. Nous rendons grâces au Seigneur qui nous a accordé une café en ville ('Caffecino') comme lieu.",
 
-                    "prayer-2" => "Les études bibliques sur le livre de Josué et l'Évangile selon Jean.",
+                    "prayer-3" => "Un étudiant fidèle de grandir comme disciple de Jésus qui a la capacité de renforcer le ministère à servir la communauté francophone.",
 
-                    "prayer-3" => "L'équipe pastorale (sagesse, esprit de prière et bonne communication).",
+                    "prayer-4" => "Deux fidèles étudiants pour Joshua et Céline chaque semaine à l'étude biblique et de grandir dans la foi. Prier pour José et de l'oeuvre de Dieu dans sa vie.",
 
-                    "prayer-4" => "La formation de disciples parmi nos jeunes adultes.",
+                    "prayer-5" => "De grandir d'une manière saine et durable, sans épuisement, sans pression malsaine ni dépendance vis-à-vis d’un petit nombre de personnes. Priez pour des structures sages, un partage des responsabilités, des familles saines, des relations solides et des fruits spirituels à long terme. Surtout pour la famille de Joshua et Céline et tous ceux qui sert avec la perseverance, patience et foi.",
 
-                    "prayer-5" => "Soutenir le ministère à Sherbrooke (Joshua et Céline Kirkland, ainsi que David et Connie Jumeau, qui se joindront au ministère en juillet).",
+                    "prayer-6" => "Ministère de musique - Que Dieu suscite des musiciens bénévoles, des chanteurs, des responsables de louange afin de contribuer à la mise en place d’un programme de musique et de louange fidèle et centré sur le Christ.",
 
-                    "prayer-6" => "La conférence francophone en juillet (préparatifs dirigés par les jeunes adultes).",
+                    "prayer-7" => "30 participants à l'activité de course chaque semaine. Le but c'est d'avoir la bonne santé, communion, la discipline physique et l'endurance spirituel.",
 
-                    "prayer-7" => "La croissance du ministère étudiant (étude biblique à l'Université Concordia, étude biblique Anchor à l'Université McGill et étude biblique à l'Université de Montréal).",
+                    "prayer-8" => "Prions pour David, Clarise, Lida, Jordan, Nyla, Witness, Jovanna, Francis, Jaleh, Tahlia et tous ceux qui ont été liés à notre ministère. Prions pour que chacun puisse grandir personnellement en Christ et répondre à l’appel de Dieu.",
 
-                    "prayer-8" => "Élizabeth Jumeau d'être une source de bénédiction à son voyage de court-terme à Bélize (26 juillet au 3 août).",
+                    "prayer-9" => "Prions pour la croissance de notre communauté afin d'acceuillir le monde avec l'hospitalité pour la gloire de Dieu.",
+
+                    "prayer-10" => "Prions pour la sagesse et la provision de Dieu.",
                     
                 ],
+                
             ],
 
 
